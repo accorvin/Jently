c43037a5baf15765ece1c354b29106646fb65b30
Alex Corvin <acorvin@localhost.rackspace.corp>
2013-07-31 15:47:41 -0400

    Fix for working with new commits

--- lib/helpers/pull_requests_data.rb
+++ lib/helpers/pull_requests_data.rb
@@ -23,14 +23,14 @@ module PullRequestsData
     if !data.has_key?(repo_name)
       data[repo_name] = Hash.new
     end
-    if !data[repo_name].has_key?(id)
-      data[repo_name][id] = pull_request_data
-    end
+    
+    data[repo_name][id] = pull_request_data
     
     data[repo_name][id][id] = id
     data[repo_name][id][:priority] = get_new_priority(repo_name, pull_request_data)
     data[repo_name][id][:is_test_required] = test_required?(repo_name, pull_request_data)
     data[repo_name][id][:status] = pull_request_data[:status]
+    data[repo_name][id][:head_sha] = pull_request_data[:head_sha]
     write(data)
   end
 
